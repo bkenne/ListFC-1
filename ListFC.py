85b87afa087364a9848b78ef0066579d72761466
bkenne <bkenne@gmail.com>
2017-09-08 01:02:55 +0000

    Completed Python Code
    The scripts coding is complete.

--- ListFC.py
+++ ListFC.py
@@ -1,21 +1,23 @@
-# Author:  
-# Date:
-# Purpose:  
+# Author:  Bryon Kenne
+# Date: 9/8/2017
+# Purpose:  Print the name of the feature class
 
 
 # Import ArcPy
 import arcpy
- 
 
 # Set the workspace
- 
+arcpy.env.workspace = r"c:\school\Assignment2\Newark.gdb"
 
-# List all of the feature classes in the Newark geodatabase 
+# List all of the feature classes in the Newark geodatabase
+datasets = arcpy.ListDatasets(feature_type='feature')
+datasets = [''] + datasets if datasets is not None else []
 
-
-# Iterate through all the feature classes and print the name of each feature class 
-
+# Iterate through all the feature classes and print the name of each feature class
+for ds in datasets:
+    for fc in arcpy.ListFeatureClasses(feature_dataset=ds):
+        print(fc)
 
 # Print "Complete"
-
+print("Complete")
 
